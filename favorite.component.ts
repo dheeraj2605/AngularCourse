c739465afbf6b10f50edfffb3f34e49bd4c58f5b
dheeraj2605 <57037913+dheeraj2605@users.noreply.github.com>
2023-09-28 14:32:57 +0530

    Add files via upload

---
+++ favorite.component.ts
@@ -0,0 +1,19 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent {
+
+  isFavorite:boolean=false;
+  
+
+  
+
+  onClick(){
+    this.isFavorite=!this.isFavorite;
+    console.log("star is getting clicked")
+  }
+}
